02178854a0eb1ad355a137eeebc836399c46112e
Lucas Damasceno <lucas_sacul12@hotmail.com>
2024-05-31 16:22:36 -0300

    update: criação de métodos GET para vários produtos e um produto

--- src/main/java/com/lucasdev/springboot/controllers/ProductController.java
+++ src/main/java/com/lucasdev/springboot/controllers/ProductController.java
@@ -1,9 +1,15 @@
 package com.lucasdev.springboot.controllers;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +19,9 @@ import com.lucasdev.springboot.model.ProductModel;
 import com.lucasdev.springboot.repositories.ProductRepository;
 
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @RestController
 public class ProductController {
@@ -27,4 +36,20 @@ public class ProductController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if(productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+    
+    
 }
